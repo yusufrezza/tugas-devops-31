2554bf022f2c6632b9d5db74203ff5058e0efc14
yusufrezza <yusuf09rezza@gmail.com>
2025-09-04 14:38:39 +0000

    perubahan beberapa file repo local

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    node: true,
+    jest: true,
+  },
+  globals: {
+    __ENV: "readonly",
+  },
+};
--- loadtest/script.js
+++ loadtest/script.js
@@ -1,3 +1,7 @@
+/* global __ENV */
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
 // loadtest/script.js
 import http from 'k6/http';
 import { sleep, check } from 'k6';
